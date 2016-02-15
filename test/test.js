6a97c361de7f196ee61fbe058b6d244e4555b73a
Johannes Terhürne <johannes@terhuerne.org>
2016-02-15 17:17:58 +0100

    First Version with working Login & Logout

---
+++ test/test.js
@@ -0,0 +1,49 @@
+var should = require('should'),
+    runtastic = require('../');
+
+describe('Constructor', function() {
+  it('Should save the credentials to the Object', function() {
+    var runtasticSession = new runtastic('username', 'password');
+    runtasticSession.username.should.equal('username');
+    runtasticSession.password.should.equal('password');
+  });
+});
+
+describe('.login(function(err, user))', function() {
+  it('Successful Login with valid credentials', function(done) {
+    var runtasticSession = new runtastic(process.env.runtasticUserName, process.env.runtasticUserPassword);
+    runtasticSession.login(function(err, user) {
+      if (err) throw err;
+      done();
+    });
+  });
+
+  it('Failed Login with invalid credentials', function(done) {
+    var runtasticSession = new runtastic('wrong', 'credentials');
+    runtasticSession.login(function(err, user) {
+      err['code'].should.equal(403);
+      done();
+    });
+  });
+
+  it('Failed Login with no credentials', function(done) {
+      var runtasticSession = new runtastic();
+      runtasticSession.login(function(err, user) {
+        err['code'].should.equal(401);
+        done();
+      });
+  });
+});
+
+describe('.logout(function(err, status))', function() {
+  it('Successful Logout after successful Login', function(done) {
+    var runtasticSession = new runtastic(process.env.runtasticUserName, process.env.runtasticUserPassword);
+    runtasticSession.login(function(err, user) {
+      if (err) throw err;
+      runtasticSession.logout(function(err, status) {
+        if (err) throw err;
+        done();
+      });
+    });
+  });
+});
